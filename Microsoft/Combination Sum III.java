212991c7d5bc6ce3dde52e47c71c5e1694f2c5ae
abhishekbansal131 <90637721+abhishekbansal131@users.noreply.github.com>
2023-01-02 20:26:52 +0530

    Add files via upload

---
+++ Microsoft/Combination Sum III.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> result = new ArrayList<>();
+        combinations(1,9,k,n,new ArrayList<>(),result);
+        return result;
+    }
+    public void combinations(int start,int end,int k,int target,List<Integer> helper,List<List<Integer>> result){
+        if(target==0 && helper.size()==k){
+                result.add(new ArrayList<>(helper));
+                return;
+        }
+
+        for(int i=start;i<=end;i++){
+            helper.add(i);
+            combinations(i+1,end,k,target-i,helper,result);                       
+            temp.remove(helper.size()-1);
+        }
+        return;
+    }
+}
